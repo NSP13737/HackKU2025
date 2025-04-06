6e6df44f30ddedabe3b44957021e54b19af262ba
NSP13737 <nathan.s.phillips@outlook.com>
2025-04-06 04:46:43 -0500

    Creating AudioTranscriber class

--- website.py
+++ website.py
@@ -1,16 +1,8 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+from AudioTranscriber import AudioTranscriber
 
-st.title("Police Dashcam Report Summary")
-st.write()
-
-num_summary = 0 #Reference later
-uploaded_file = st.file_uploader("Choose a JSON file", type="json")
-
-if uploaded_file is not None:
-    st.write("File uploaded... ")
-
-    st.subheader("Summary", num_summary)
-    if st.button("Generate Recording"):
-        st.audio_input()
+#Example below
+transcriber = AudioTranscriber()
+transcriber.start_processing()
+transcriber.stop_processing()
+st.write(f"Transcript saved at: {transcriber.transcript}")
